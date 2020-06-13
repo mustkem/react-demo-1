5d8e7cd6a0e9d5ed4de0e94601c1b1d95612b8af
mustkeem <vikash@csschopper.com>
2020-06-13 14:23:54 +0530

    Updates

---
+++ precache-manifest.305304add932a73d7d09f936e77e1b78.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0d611d626e48c995c2900395d05fb88",
+    "url": "/react-workshop-indegene/index.html"
+  },
+  {
+    "revision": "655caf6379c3bc355e68",
+    "url": "/react-workshop-indegene/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "153dce25e7a258e3d51f",
+    "url": "/react-workshop-indegene/static/css/main.862f648a.chunk.css"
+  },
+  {
+    "revision": "655caf6379c3bc355e68",
+    "url": "/react-workshop-indegene/static/js/2.f3178dab.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-workshop-indegene/static/js/2.f3178dab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "153dce25e7a258e3d51f",
+    "url": "/react-workshop-indegene/static/js/main.a889839f.chunk.js"
+  },
+  {
+    "revision": "fb9e3e1c488b2a0ae716",
+    "url": "/react-workshop-indegene/static/js/runtime-main.c46af51c.js"
+  }
+]);
