38501174ac19998fefe34971ff0133b331fdea90
mustkem <mo.mustkem@gmail.com>
2020-06-15 04:39:43 +0530

    add

---
+++ build/precache-manifest.9966872ba54bebafe5ce152dbee158c6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecd3feaed8c16f6006d4db36339837c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f1612d7b9e3d138b5d11",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "ff00b7cac37b5943e034",
+    "url": "/static/css/main.6ec52207.chunk.css"
+  },
+  {
+    "revision": "f1612d7b9e3d138b5d11",
+    "url": "/static/js/2.495188b4.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.495188b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff00b7cac37b5943e034",
+    "url": "/static/js/main.f65db74d.chunk.js"
+  },
+  {
+    "revision": "216563cf50e7c35b2f13",
+    "url": "/static/js/runtime-main.7c0ae09a.js"
+  }
+]);
